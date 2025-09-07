9930899c8c8faf4fef35fa436c170a09babcb90c
LobbyTech <1187755698@qq.com>
2025-09-08 03:58:40 +0800

    getName卡服？

--- src/main/java/com/xzavier0722/mc/plugin/slimeglue/module/KingdomsXModule.java
+++ src/main/java/com/xzavier0722/mc/plugin/slimeglue/module/KingdomsXModule.java
@@ -1,14 +1,14 @@
 package com.xzavier0722.mc.plugin.slimeglue.module;
 
-import com.xzavier0722.mc.plugin.slimeglue.api.ACompatibilityModule;
-import com.xzavier0722.mc.plugin.slimeglue.api.protection.IBlockProtectionHandler;
-import com.xzavier0722.mc.plugin.slimeglue.api.protection.IPlayerProtectionHandler;
 import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.plugin.Plugin;
-import org.kingdoms.constants.group.Kingdom;
 import org.kingdoms.constants.land.Land;
 import org.kingdoms.constants.land.location.SimpleChunkLocation;
+
+import com.xzavier0722.mc.plugin.slimeglue.api.ACompatibilityModule;
+import com.xzavier0722.mc.plugin.slimeglue.api.protection.IBlockProtectionHandler;
+import com.xzavier0722.mc.plugin.slimeglue.api.protection.IPlayerProtectionHandler;
 
 public class KingdomsXModule extends ACompatibilityModule {
 
@@ -16,19 +16,19 @@ public class KingdomsXModule extends ACompatibilityModule {
         addProtectionHandler(new IBlockProtectionHandler() {
             @Override
             public boolean canPlaceBlock(OfflinePlayer player, Location location) {
-                verbose("canPlaceBlock: player=" + player.getName() + ", loc=" + location);
+                //verbose("canPlaceBlock: player=" + player.getName() + ", loc=" + location);
                 return canAccess(player, location);
             }
 
             @Override
             public boolean canBreakBlock(OfflinePlayer player, Location location) {
-                verbose("canBreakBlock: player=" + player.getName() + ", loc=" + location);
+                //verbose("canBreakBlock: player=" + player.getName() + ", loc=" + location);
                 return canAccess(player, location);
             }
 
             @Override
             public boolean canInteractBlock(OfflinePlayer player, Location location) {
-                verbose("canInteractBlock: player=" + player.getName() + ", loc=" + location);
+                //verbose("canInteractBlock: player=" + player.getName() + ", loc=" + location);
                 return canAccess(player, location);
             }
         });
@@ -36,19 +36,19 @@ public class KingdomsXModule extends ACompatibilityModule {
         addProtectionHandler(new IPlayerProtectionHandler() {
             @Override
             public boolean canAttackPlayer(OfflinePlayer player, Location location) {
-                verbose("canAttackPlayer: player=" + player.getName() + ", loc=" + location);
+                //verbose("canAttackPlayer: player=" + player.getName() + ", loc=" + location);
                 return canAccess(player, location);
             }
 
             @Override
             public boolean canAttackEntity(OfflinePlayer player, Location location) {
-                verbose("canAttackEntity: player=" + player.getName() + ", loc=" + location);
+                //verbose("canAttackEntity: player=" + player.getName() + ", loc=" + location);
                 return canAccess(player, location);
             }
 
             @Override
             public boolean canInteractEntity(OfflinePlayer player, Location location) {
-                verbose("canInteractEntity: player=" + player.getName() + ", loc=" + location);
+                //verbose("canInteractEntity: player=" + player.getName() + ", loc=" + location);
                 return canAccess(player, location);
             }
         });
@@ -56,16 +56,19 @@ public class KingdomsXModule extends ACompatibilityModule {
 
     private boolean canAccess(OfflinePlayer p, Location l) {
         Land land = SimpleChunkLocation.of(l).getLand();
-        verbose("canAccess: " + land);
+        //verbose("canAccess: " + land);
         if (land == null || !land.isClaimed()) {
             return true;
         }
+        return true;
 
+        /*
         Kingdom kingdom = land.getKingdom();
         return verbose(
                 "canAccess: ret=",
                 kingdom == null || p.getUniqueId().equals(kingdom.getKingId()) || kingdom.isMember(p)
         );
+        */
     }
 
     @Override
